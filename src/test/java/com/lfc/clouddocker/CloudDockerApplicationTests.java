9e65b175423d861cd1df402490991ac69fa421ad
Fucheng_lai <fucheng_lai@163.com>
2025-03-31 14:33:24 +0800

    项目初始化

---
+++ src/test/java/com/lfc/clouddocker/CloudDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lfc.clouddocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudDockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
