988c4186a4615fc807868b62301b2ed5208d3587
Fucheng_lai <fucheng_lai@163.com>
2025-09-29 11:32:53 +0800

    增加了缺失的YunOrderMapper文件

---
+++ src/main/java/com/lfc/clouddocker/mapper/YunOrderMapper.java
@@ -0,0 +1,18 @@
+package com.lfc.clouddocker.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.lfc.clouddocker.model.entity.YunOrder;
+
+/**
+* @author ceng
+* @description 针对表【yun_order(充值订单表)】的数据库操作Mapper
+* @createDate 2025-05-17 17:10:24
+* @Entity com.lfc.clouddocker.model.entity.YunOrder
+*/
+public interface YunOrderMapper extends BaseMapper<YunOrder> {
+
+}
+
+
+
+
