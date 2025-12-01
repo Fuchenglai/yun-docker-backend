8f63a8eb9e5b67f921a63c63376f9014541842ea
laifucheng <fucheng_lai@163.com>
2025-12-01 23:14:57 +0800

    1.common模块引入redis依赖 2.common模块解决找不到类的异常

--- yun-docker-common/src/main/java/com/lfc/common/util/SpringContextUtils.java
+++ yun-docker-common/src/main/java/com/lfc/common/util/SpringContextUtils.java
@@ -1,6 +1,6 @@
 package com.lfc.common.util;
 
-import com.lfc.yundocker.constant.ServiceNameConstants;
+import com.lfc.common.constant.ServiceNameConstants;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
